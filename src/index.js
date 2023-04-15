2c36f596a5e21b9f9b52b3c26cbc4baa6862ca23
Khomotso Mkansi <kgomotsomkansi@gmail.com>
2023-04-15 13:10:03 +0200

    Fix import link

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import Store from './redux/ConfigureStore';
+import Store from './redux/configureStore';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
